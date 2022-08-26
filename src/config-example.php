d918c837115f04223328ab4265a3a97414148435
Zero <zeroliu@chipbond.com.tw>
2022-08-26 11:26:33 +0800

    update config-example.php

--- src/config-example.php
+++ src/config-example.php
@@ -8,8 +8,28 @@ return [
             'HashKey' => '5294y06JbISpM5x9',
             'HashIV' => 'v77hoKGq4kWxNNIS'
         ],
-        'invoiceUrls' => [
-            'baseUrl' => 'https://einvoice.ecpay.com.tw/B2CInvoice'
+        'B2B' => [
+            'invoiceURLs' => [
+                'base' => 'https://einvoice.ecpay.com.tw/B2BInvoice'
+            ]
+        ],
+        'B2C' => [
+            'invoiceURLs' => [
+                'base' => 'https://einvoice.ecpay.com.tw/B2CInvoice',
+                'issue' => '/issue',
+                'invalid' => '/Invalid',
+                'allowanceInvalid' => '/AllowanceInvalid',
+                'allowanceInvalidByCollegiate' => '/AllowanceInvalidByCollegiate',
+                'getIssue' => '/GetIssue',
+                'getAllowanceList' => '/GetAllowanceList',
+                'getInvalid' => '/GetInvalid',
+                'getAllowanceInvalid' => '/GetAllowanceInvalid',
+                'getInvoiceWordSetting' => '/GetInvoiceWordSetting',
+                'invoiceNotify' => '/InvoiceNotify',
+                'invoicePrint' => '/InvoicePrint',
+                'checkBarcode' => '/CheckBarcode',
+                'checkLoveCode' => '/CheckLoveCode',
+            ]
         ]
     ]
 ];
